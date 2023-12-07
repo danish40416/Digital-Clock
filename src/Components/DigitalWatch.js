0c1c629e5accf5f9a36b4aa071237e663acec1df
Danish <haseebasif507@gmail.com>
2023-12-07 15:50:20 +0500

    Project Done!

---
+++ src/Components/DigitalWatch.js
@@ -0,0 +1,26 @@
+import React  from 'react';
+import Hours from './Hours';
+import Minutes from './Minutes';a
+import Seconds from './Seconds';
+
+export default function DigitalWatch({ time, setTime }) {
+
+  return (
+  <div className="app">
+    <div className="container">
+      <div className="box bg-primary">
+        <Hours time={time} setTime={setTime} />
+      </div>
+      <div className='colon' >:</div>
+
+      <div className="box bg-primary">
+        <Minutes time={time} setTime={setTime} />
+      </div>
+      <div className='colon' >:</div>
+      <div className="box bg-info">
+        <Seconds time={time} setTime={setTime} />
+      </div>
+    </div>
+  </div>
+  );
+}
