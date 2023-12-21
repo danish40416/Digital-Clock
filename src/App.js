814fb79106d6489c2d8551ac279f826f23611e4b
Danish <haseebasif507@gmail.com>
2023-12-21 16:50:59 +0500

    styling of watch

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DigitalWatch from './Components/digitalClock/DigitalWatch';
 import './App.css';
 
 export default function App() {
-  const [time, setTime] = useState({ seconds: 0, minutes: 0, hours: 0 });
  
-  const resetTime = () => {
-    setTime({ seconds: 0, minutes: 0, hours: 0 });
-  };
   return (
-    <>
-    <div className="app">
-      <div className="container">
-        <DigitalWatch time={time} setTime={setTime} />
-      </div>
-    <button className="reset-btn " onClick={resetTime}> Reset </button>
-    </div>
-    
-    </>
+    <div> <DigitalWatch /></div>
   );
 }
