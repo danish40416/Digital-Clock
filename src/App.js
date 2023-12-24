a73b5070337f92829df3183c2370d1312b40f144
Danish <haseebasif507@gmail.com>
2023-12-24 11:36:40 +0500

    abc

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import DigitalWatch from './Components/digitalClock/DigitalWatch';
 import './App.css';
 
 export default function App() {
+  console.log('abc')
 
   return (
     <div> <DigitalWatch /></div>
+
   );
 }
