287ee1eb8a3f6b9a933f31d23ec609d70d78011e
Danish <haseebasif507@gmail.com>
2023-12-25 18:52:49 +0500

    adjust clock hands

--- src/Components/analogClock/AnalogWatch.js
+++ src/Components/analogClock/AnalogWatch.js
@@ -10,9 +10,24 @@ export default function AnalogWatch() {
       <div className={classes.num} id={classes.six}>6</div>
       <div className={classes.num} id={classes.nine}>9</div>
       <div className={classes.num} id={classes.twelve}>12</div>
-      <div className={classes.origin} >
-        <div className={classes.hour}></div>
+      <div className={classes.origin} >.</div>
 
+      <div className={classes.hoursWrapper}>
+        <div className={classes.hoursHand}>
+          <div className={classes.hand}></div>
+        </div>
+      </div>
+
+      <div class={classes.minutesWrapper}>
+        <div class={classes.minutesHand}>
+          <div class={classes.hand}></div>
+        </div>
+      </div>
+
+      <div class={classes.secondsWrapper}>
+        <div class={classes.secondsHand}>
+          <div class={classes.hand}></div>
+        </div>
       </div>
     </div>
   </div>
