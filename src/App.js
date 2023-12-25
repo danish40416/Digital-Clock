c02f7eec8ed6d047b0b045fecaedd51635969d75
Danish <haseebasif507@gmail.com>
2023-12-25 11:46:42 +0500

    styling analog clock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import DigitalWatch from './Components/digitalClock/DigitalWatch';
+import AnalogWatch from './Components/analogClock/AnalogWatch';
 import './App.css';
 
 export default function App() {
   console.log('abc')
 
   return (
-    <div> <DigitalWatch /></div>
+    <div> 
+       {/* <DigitalWatch /> */}
+       <AnalogWatch/>
+    </div>
 
   );
 }
