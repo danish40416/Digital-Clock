922c05f7ebb7b5ff867076bb8b52b0a44bab21d9
Danish <haseebasif507@gmail.com>
2023-12-25 16:41:17 +0500

    Clock House

---
+++ src/Components/clockHouse/ClockHouse.js
@@ -0,0 +1,16 @@
+// ClockHouse.js
+import React from 'react';
+import classes from './ClockHouse.module.css';
+import { Link } from 'react-router-dom';
+
+export default function ClockHouse() {
+ 
+
+
+  return (
+    <div className={classes.container}>
+      <h1 className={classes.heading}>Welcome to Clock House </h1>
+      <Link className={classes.house} to='/see-watches'>Cick here to see Watches</Link>
+    </div>
+  );
+}
