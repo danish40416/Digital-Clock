13d20a4f1ac4c502cd6d304b29850dca08a7b2a2
Danish <haseebasif507@gmail.com>
2023-12-27 10:40:23 +0500

    modify the code

--- src/Components/digitalClock/Hours.js
+++ src/Components/digitalClock/Hours.js
@@ -1,1 +1,9 @@
 
+import React  from 'react';
+
+export default function Hours({ time }) {
+    const newHours = time.Hours < 10  ? `0${time.Hours}`: time.Hours
+
+    return <div>{newHours}</div>;
+}
+
