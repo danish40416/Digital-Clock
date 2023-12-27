c12375e61ee488d7054b7c230dd0fd6b047d6698
Danish <haseebasif507@gmail.com>
2023-12-27 13:42:54 +0500

    modification in CSS

--- src/Components/reusable/Buttons.js
+++ src/Components/reusable/Buttons.js
@@ -1,17 +1,14 @@
 import React from 'react';
+import classes from './Buttons.module.css'
+
+const {openModalBtn, resetBtn, updateRealTimeBtn, buttons}= classes
 
 const Buttons = ({ openModal, resetTime, updateRealTime }) => {
   return (
-    <div className='buttons'>
-      <button className='openModalBtn' onClick={openModal}>
-        Set Time
-      </button>
-      <button className='resetBtn' onClick={resetTime}>
-        Reset
-      </button>
-      <button className='updateRealTimeBtn' onClick={updateRealTime}>
-        Update Real Time
-      </button>
+    <div className={buttons}>
+      <button className={openModalBtn} onClick={openModal}> Set Time</button>
+      <button className={resetBtn} onClick={resetTime}>  Reset </button>
+      <button className={updateRealTimeBtn} onClick={updateRealTime}> Update Real Time </button>
     </div>
   );
 };
